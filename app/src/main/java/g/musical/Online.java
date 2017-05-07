0be0f2c1994f070d159cf8eec9ef7634042390d3
G <gremski@gmail.com>
2017-05-07 23:14:24 +0200

    remade to work on instances with custom bottom bar

--- app/src/main/java/g/musical/Online.java
+++ app/src/main/java/g/musical/Online.java
@@ -2,62 +2,74 @@ package g.musical;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.View;
 
 
 public class Online extends AppCompatActivity {
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_local:
-                    switchToLocal();
-                    return true;
-                case R.id.navigation_lyrics:
-                    switchToLyrics();
-                    return true;
-                case R.id.navigation_notifications:
-                    switchToNotifications();
-                    return true;
-                case R.id.navigation_playing:
-                    switchToPlaying();
-                    return true;
-                case R.id.navigation_online:
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        navigation.setSelectedItemId(R.id.navigation_online);
+
+        final View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button_local:
+                        switchToLocal();
+                        break;
+                    case R.id.button_lyrics:
+                        switchToLyrics();
+                        break;
+                    case R.id.button_notifications:
+                        switchToNotifications();
+                        break;
+                    case R.id.button_playing:
+                        switchToPlaying();
+                        break;
+                    case R.id.button_prev:
+                        // play previous song
+                        break;
+                    case R.id.button_next:
+                        // play next song
+                        break;
+                    case R.id.button_play_pause:
+                        // play or pause current song
+                        break;
+                }
+            }
+        };
+
+        findViewById(R.id.button_playing).setOnClickListener(clickListener);
+        findViewById(R.id.button_local).setOnClickListener(clickListener);
+        findViewById(R.id.button_lyrics).setOnClickListener(clickListener);
+        findViewById(R.id.button_next).setOnClickListener(clickListener);
+        findViewById(R.id.button_notifications).setOnClickListener(clickListener);
+        findViewById(R.id.button_online).setOnClickListener(clickListener);
+        findViewById(R.id.button_play_pause).setOnClickListener(clickListener);
+        findViewById(R.id.button_prev).setOnClickListener(clickListener);
     }
+
+
     public void switchToLocal() {
         Intent intent = new Intent(this, Local.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         startActivity(intent);
     }
+
+    public void switchToPlaying() {
+        Intent intent = new Intent(this, Playing.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+    }
+
     public void switchToLyrics() {
         Intent intent = new Intent(this, Lyrics.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         startActivity(intent);
     }
+
     public void switchToNotifications() {
         Intent intent = new Intent(this, Notifications.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(intent);
-    }
-    public void switchToPlaying() {
-        Intent intent = new Intent(this, Playing.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         startActivity(intent);
     }
 
